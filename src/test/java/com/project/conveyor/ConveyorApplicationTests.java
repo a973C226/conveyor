453bb0086a7309df3232b7636049058de873c7eb
Anton <cde120502@yandex.ru>
2022-12-14 03:25:23 +0400

    Initial commit In pom.xml: lombok, openapi-ui, springboot DevTools

---
+++ src/test/java/com/project/conveyor/ConveyorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.conveyor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConveyorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
