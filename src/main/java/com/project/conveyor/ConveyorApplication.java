367ec72cd8787ab636769f2ed4c07956c73d2976
Anton <cde120502@yandex.ru>
2022-12-14 15:42:55 +0400

    Удалил баннер в верхней части файла журнала

--- src/main/java/com/project/conveyor/ConveyorApplication.java
+++ src/main/java/com/project/conveyor/ConveyorApplication.java
@@ -1,10 +1,15 @@
 package com.project.conveyor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class ConveyorApplication {
+
+	static final Logger logger = LoggerFactory.getLogger(ConveyorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConveyorApplication.class, args);
